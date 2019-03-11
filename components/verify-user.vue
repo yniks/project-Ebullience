bcb1f2d5cec05f9423880079951594878361c85e
swapnil-tiwari <swapnil_tiwari@outlook.com>
2019-03-12 00:19:06 +0530

    verifyvue

---
+++ components/verify-user.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="enter-roll-box">
+    <div id=welcome-text>Is it your first visit? / Are you lost?
+    </div>
+    <div id="inputroll">
+      <div class="label" id="roll-label">Your roll number</div>
+      <input id="input-roll-box" />
+    </div>
+    <div id="securityQuestion">
+      <div class="label" id="roll-label">Your roll number</div>
+      <input id="input-roll-box" />
+    </div>
+    <button id="submit">Next</button>
+  </div>
+</template>

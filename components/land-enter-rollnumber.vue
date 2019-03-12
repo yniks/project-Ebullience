5f20a1f6d4b45b79c034c71a86f0a486bc81083d
Nikhilesh Yadav <nikhileshofficial@gmail.com>
2019-03-12 21:54:26 +0530

    custom styles and tests

--- components/land-enter-rollnumber.vue
+++ components/land-enter-rollnumber.vue
@@ -1,27 +1,21 @@
 <template>
-  <div id="enter-roll-box">
-    <div id=welcome-text>Welcome
-    </div>
+  <div id="main-roll-box">
+    <div id=welcome-text>Welcome</div>
     <div id="label-input-container">
       <labeled-input class=".input-and-label" placeholder="Roll Number" label="Enter Roll Number :" along="vertical" ></labeled-input>
-      <labeled-input class=".input-and-label" placeholder="Secrete" label="Enter Secrete:" along="vertical" type="password"></labeled-input>
-    </div>
+      </div>
     <div id="submit-button">
-      <button id=button-submit class="surface_2 interactive">Next</button>
+      <ebul-button>Next</ebul-button>
     </div>
   </div>
 </template>
 <script>
 import labeledInput from "./labeled-input";
+import ebulButton from "./button"
 export default {
-  name: 'HelloWorld',
   components:{
-    labeledInput
-  },
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
+    labeledInput,
+    ebulButton
   }
 }
 </script>
@@ -29,38 +23,19 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 @import url(https://fonts.googleapis.com/css?family=Aclonica);
-  #enter-roll-box
+  #main-roll-box
   {
     display: grid;
-    width:90%;
+    width:100%;
     height: 100%;
     text-align: center;
     align-content: center;
     padding: 20px;
-    grid-template-rows: 1.5fr 2fr 1fr;
+    grid-template-rows: 1.5fr 1.6fr 1fr;
     background: rgba(255,255,255,0.5);
     box-shadow:0px 0px 9px rgba(61,5,27,0.54);
     border: 0px;
     border-radius: 15px;
-  }
-  #input-roll-label
-  {
-    text-align: left;
-    padding-bottom: 12px;
-    color: #115555;
-    font-size: 1.2em
-  }
-  #input-roll
-  {
-    height: 3em;
-    opacity: 0.75;
-  }
-  #label-input-container
-  {
-      display: flex;
-      flex-direction: column;
-      justify-content: space-evenly;
-      padding:12px;
   }
   .input-and-label
   {
@@ -78,12 +53,6 @@ export default {
   {
     text-align: right;
     padding: 12px;
-  }
-  #button-submit
-  {
-    height:2.7em;
-    min-width: 5em;
-    width: 7vw;
   }
 
 </style>
